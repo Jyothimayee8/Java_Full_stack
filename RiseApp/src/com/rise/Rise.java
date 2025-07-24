dff6cc72a3bc2db4036885efff6b6ffbc88416eb
Jyothimayee8 <146567709+Jyothimayee8@users.noreply.github.com>
2025-07-24 15:38:25 +0530

    Add files via upload

---
+++ RiseApp/src/com/rise/Rise.java
@@ -0,0 +1,15 @@
+package com.rise;
+
+public class Rise {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO RISE ENGINEERING COLLEGE");
+		System.out.print("Please register with your details");
+		Scanner sc = new Scanner(Sys)
+		System.out.println();
+		
+		System.out.println();
+	}
+
+}
